e8f21ffc919a1270c04b1db7334b3e77091161f5
waiga <hyang9387@gmail.com>
2022-03-29 22:55:05 +0900

    error

--- Quiz_Prototype1/AppDelegate.swift
+++ Quiz_Prototype1/AppDelegate.swift
@@ -9,11 +9,21 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    //이 부분 없이 하단 func 작성하다 보니 문제가 생겼음.
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = self.window {
+            window.backgroundColor = UIColor.white
+            
+            let nav = UINavigationController()
+            let mainView = ViewController()
+            nav.viewControllers = [mainView]
+            window.rootViewController = nav
+            window.makeKeyAndVisible()
+        }
         return true
     }
 
